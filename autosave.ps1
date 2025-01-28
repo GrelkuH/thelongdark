a951b69bdf1d80180716df11841f30f470727338
GrelkuH <99175849+GrelkuH@users.noreply.github.com>
2025-01-28 16:51:57 +0300

    Create autosave.ps1

---
+++ autosave.ps1
@@ -0,0 +1,13 @@
+#Powershell script for Task Scheduler 
+
+# Edit Custom Variables for source and destination directories before start 
+    $sourcedir = 'C:\Users\*username*\AppData\Local\Hinterland\TheLongDark\Survival\'
+    $sourcefilename = 'sandbox1'    
+    $destpath   = 'C:\Backup\autosave\'
+#    
+    $sourcepath = $sourcedir + $sourecfilename
+
+    $sb = {Copy-Item -Path $sourcepath -Destination ($destpath + $sourcefilename + '_'+ (Get-Date -f dd.MM_HH-mm-ss))}
+
+    Register-EngineEvent -SourceIdentifier "MyEvent" -Action $sb
+    New-FileSystemWatcher -SourceIdentifier "MyEvent" -Path $sourcedir -NotifyFilter LastWrite -Filter $sourecfilename
